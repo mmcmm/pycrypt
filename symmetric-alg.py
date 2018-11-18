46d77a6ab15cf6292206206822fbab5ecfc40d41
mtdx <ms2asdk@gmail.com>
2018-11-18 15:10:09 +0200

    encrypt

--- symmetric-alg.py
+++ symmetric-alg.py
@@ -1,49 +1,52 @@
 
 def save_file(results, file_path):
-    # Create File Object
-    f = open (file_path, 'w')
-    # Write Results
-    f.write (results)
-    # Close File Object
-    f.close ()
+    f = open(file_path, 'w')
+    f.write(results)
+    f.close()
 
 
 def open_file(file_path):
-    # Create File Object
-    f = open (file_path, 'r')
-    # Read File Contents
-    file_contents = f.read ()
-    # Close File Object
-    f.close ()
+    f = open(file_path, 'r')
+    file_contents = f.read()
+    f.close()
 
     return file_contents
 
 def calculate_key(key):
     results = 0
     counter = 0
-    # Convert each Character into an INT and added to results
     for char in key:
         counter += 1
-        results += ord (char)
+        results += ord(char)
 
-    # Return the results divided by the number of Characters in the key
-    return int (results / counter)
+    return int(results / counter)
 
+def encrypt(file_path, key):
+    file_contents = open_file(file_path)
+    key_calc = calculate_key(key)
+    enc_results = ''
+
+    for line in file_contents:
+        for wrd in line:
+            for char in wrd:
+                int_char = ord(char) + key_calc
+                enc_results += chr(int_char)
+
+    save_file(enc_results, file_path)
+    print '[!] Finished Encryption'
 
 def main():
-    print('[1] Encrypt \n [2] Decrypt')
+    print '[1] Encrypt \n [2] Decrypt'
     choice = raw_input('>')
-    print('File Path: ')
+    print 'File Path: '
     file_path = raw_input ('>')
     print 'Secret Key: '
-    key = raw_input ('>')
-    # Encrypt
-    if choice == "1":
-        encrypt (file_path, key)
-    # Decrypt
-    elif choice == "2":
-        decrypt (file_path, key)
-
+    key = raw_input('>')
+   
+    if choice == "1":   # Encrypt
+        encrypt(file_path, key) 
+    elif choice == "2":  # Decrypt
+        decrypt(file_path, key)
     else:
         print 'Invalid Choice'
 
