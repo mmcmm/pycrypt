d5e3f124169ccc1df6bfe24698bc1c5b9e283800
mtdx <ms2asdk@gmail.com>
2018-11-18 14:52:24 +0200

    add main method entry point

--- symmetric-alg.py
+++ symmetric-alg.py
@@ -1,1 +1,22 @@
-    
+
+
+
+def main():
+    print('[1] Encrypt \n [2] Decrypt')
+    choice = raw_input('>')
+    print('File Path: ')
+    file_path = raw_input ('>')
+    print 'Secret Key: '
+    key = raw_input ('>')
+    # Encrypt
+    if choice == "1":
+        encrypt (file_path, key)
+    # Decrypt
+    elif choice == "2":
+        decrypt (file_path, key)
+
+    else:
+        print 'Invalid Choice'
+
+if __name__ == '__main__':
+    main ()
