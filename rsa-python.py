29d3b5ec9cbd88e4c7c14aa2abb57541fdf75ca0
mtdx <ms2asdk@gmail.com>
2018-11-26 18:28:05 +0200

    encrypt function

--- rsa-python.py
+++ rsa-python.py
@@ -27,7 +27,16 @@ class CryptoRSA:
     def generate_keys(self):
         keys = RSA.generate(4096)
         private_key = keys.exportKey("PEM")
-        public_key = keys.publickey().exportKey ("PEM")
-        self.__save_file(private_key,self.PRIVATE_KEY_FILE)
-        self.__save_file(public_key,self.PUBLIC_KEY_FILE)
+        public_key = keys.publickey().exportKey("PEM")
+        self.__save_file(private_key, self.PRIVATE_KEY_FILE)
+        self.__save_file(public_key, self.PUBLIC_KEY_FILE)
         print "Public & Private Keys; generated & saved successfully!"  
+
+    def encrypt(self,cleartext, public_keypath=None):
+        if(public_keypath==None):
+            public_keypath = self.PUBLIC_KEY_FILE
+            
+        public_key = RSA.importKey(self.__read_file(public_keypath))
+        cipher = PKCS1_OAEP.new(public_key)
+        encrypted_data = cipher.encrypt(cleartext)
+        return base64.b64encode(encrypted_data)    
